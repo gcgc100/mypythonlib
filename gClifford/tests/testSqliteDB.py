8ceebd584b6c0b5813266bba7c7fc6f016315b06
Chong Guan <gc771427477@gmail.com>
2017-08-31 16:01:43 -0400

    Add sqliteDB

---
+++ gClifford/tests/testSqliteDB.py
@@ -0,0 +1,16 @@
+import unittest
+import sqliteDB
+
+
+class TestSqliteDB(unittest.TestCase):
+
+    """Test case docstring."""
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_name(self):
+        pass
